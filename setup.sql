b8f81ed41ef658c697e300ec07489c4f29b85243
Ryen21 <33225286+Ryen21@users.noreply.github.com>
2025-07-31 13:32:42 -0400

    Setup package.json and create setup.sql script

---
+++ setup.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE corked;
+\c corked
+CREATE TABLE IF NOT EXISTS reviews {
+    id SERIAL PRIMARY KEY,
+    wine_name VARCHAR(30) NOT NULL,
+    wine_description VARCHAR(100) NOT NULL,
+    score INT NOT NULL,
+    CHECK (score >= 0 AND score <= 10)
+}
