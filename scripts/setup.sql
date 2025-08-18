4711ad756e318777b94524036a4ac2a99fee0e01
Noelle Hii <forcozydays@gmail.com>
2025-08-18 00:39:35 -0400

    changed local table cols in reviews to match AWS

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS saved_wines (
 
 CREATE TABLE IF NOT EXISTS reviews (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    wine_id INT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    wine_name VARCHAR(255) NOT NULL,
     post_description TEXT,
     score INT NOT NULL CHECK (score >= 0 AND score <= 10)
 );
